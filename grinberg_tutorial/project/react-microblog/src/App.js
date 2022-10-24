6f4197787de9b925a42b79f0484a1c0b48a8b9e2
Carl Hinderer <carl.hinderer4@gmail.com>
2022-10-24 12:52:29 -0600

    Added Header component.

--- grinberg_tutorial/project/react-microblog/src/App.js
+++ grinberg_tutorial/project/react-microblog/src/App.js
@@ -1,3 +1,6 @@
+import Container from 'react-bootstrap/Container';
+import Header from './components/Header';
+
 export default function App() {
   const posts = [
     {
@@ -19,21 +22,23 @@ export default function App() {
   ];
 
   return (
-    <>
-      <h1>Microblog</h1>
-      {posts.length === 0 ?
-        <p>There are no blog posts.</p>
-      :
-        posts.map(post => {
-          return (
-            <p key={post.id}>
-              <b>{post.author.username}</b> &mdash; {post.timestamp}
-              <br />
-              {post.text}
-            </p>
-          );
-        })
-      }
-    </>
+    <Container fluid className="App">
+      <Header />
+      <Container>
+        {posts.length === 0 ?
+          <p>There are no blog posts.</p>
+        :
+          posts.map(post => {
+            return (
+              <p key={post.id}>
+                <b>{post.author.username}</b> &mdash; {post.timestamp}
+                <br />
+                {post.text}
+              </p>
+            );
+          })
+        }
+      </Container>
+    </Container>
   );
 }
