e2727c9b348411011090e141abb72be045bde81f
Carl Hinderer <carl.hinderer4@gmail.com>
2022-10-30 09:19:44 -0600

    Added rendering of posts on the User Profile and Explore pages.

--- grinberg_tutorial/project/react-microblog/src/pages/UserPage.js
+++ grinberg_tutorial/project/react-microblog/src/pages/UserPage.js
@@ -5,6 +5,7 @@ import Spinner from 'react-bootstrap/Spinner';
 import { useParams } from 'react-router-dom';
 import Body from '../components/Body';
 import TimeAgo from '../components/TimeAgo';
+import Posts from '../components/Posts';
 import { useApi } from '../contexts/ApiProvider';
 
 export default function UserPage() {
@@ -26,20 +27,23 @@ export default function UserPage() {
       :
         <>
           {user === null ?
-            <p>User not found.</p>
+            <p>Could not retrieve blog posts.</p>
           :
-            <Stack direction="horizontal" gap={4}>
-              <Image src={user.avatar_url + '&s=128'} roundedCircle />
-              <div>
-                <h1>{user.username}</h1>
-                {user.about_me && <h5>{user.about_me}</h5>}
-                <p>
-                  Member since: <TimeAgo isoDate={user.first_seen} />
-                  <br />
-                  Last seen: <TimeAgo isoDate={user.last_seen} />
-                </p>
-              </div>
+            <>
+              <Stack direction="horizontal" gap={4}>
+                <Image src={user.avatar_url + '&s=128'} roundedCircle />
+                <div>
+                  <h1>{user.username}</h1>
+                  {user.about_me && <h5>{user.about_me}</h5>}
+                  <p>
+                    Member since: <TimeAgo isoDate={user.first_seen} />
+                    <br />
+                    Last seen: <TimeAgo isoDate={user.last_seen} />
+                  </p>
+                </div>
             </Stack>
+            <Posts content={user.id} />
+            </>
           }
         </>
       }
