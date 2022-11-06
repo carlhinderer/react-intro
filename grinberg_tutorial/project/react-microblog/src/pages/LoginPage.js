7721599e30e41902b8ad5fc00e7010b9223cdfde
Carl Hinderer <carl.hinderer4@gmail.com>
2022-11-06 01:19:25 -0600

    Added client-side validation to the login page.

--- grinberg_tutorial/project/react-microblog/src/pages/LoginPage.js
+++ grinberg_tutorial/project/react-microblog/src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Body from '../components/Body';
@@ -6,22 +6,43 @@ import InputField from '../components/InputField';
 
 export default function LoginPage() {
   const [formErrors, setFormErrors] = useState({});
+  const usernameField = useRef();
+  const passwordField = useRef();
+
+  useEffect(() => {
+    usernameField.current.focus();
+  }, []);
 
   const onSubmit = (ev) => {
     ev.preventDefault();
-    console.log('handle form here');
-  };
+    const username = usernameField.current.value;
+    const password = passwordField.current.value;
 
+    const errors = {};
+    if (!username) {
+      errors.username = 'Username must not be empty.';
+    }
+    if (!password) {
+      errors.password = 'Password must not be empty.';
+    }
+    setFormErrors(errors);
+    if (Object.keys(errors).length > 0) {
+      return;
+    }
+
+    // TODO: log the user in
+  };
+  
   return (
     <Body>
       <h1>Login</h1>
       <Form onSubmit={onSubmit}>
         <InputField
           name="username" label="Username or email address"
-          error={formErrors.username} />
+          error={formErrors.username} fieldRef={usernameField} />
         <InputField
           name="password" label="Password" type="password"
-          error={formErrors.password} />
+          error={formErrors.password} fieldRef={passwordField} />
         <Button variant="primary" type="submit">Login</Button>
       </Form>
     </Body>
